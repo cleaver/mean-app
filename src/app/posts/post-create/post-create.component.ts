c431deb988e1132d65051c381c13d15a79b3601d
Cleaver Barnes <cbarnes@cleaver.ca>
2020-09-21 16:10:28 -0400

    create post-create component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { ArrayType } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = '- empty -';
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
