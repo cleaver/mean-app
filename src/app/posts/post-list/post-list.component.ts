59900cebe275d56de38047d71388db991f3244c7
Cleaver Barnes <cbarnes@cleaver.ca>
2020-10-07 13:30:43 -0400

    add basic pagination

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
@@ -10,8 +11,12 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit, OnDestroy {
+  currentPage = 1;
   isLoading = false;
+  pageSizeOptions = [1, 2, 3, 5, 8, 13, 21];
   posts: Post[] = [];
+  postsPerPage = 2;
+  totalPosts = 10;
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -20,9 +25,15 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.postsService.deletePost(postId);
   }
 
+  onChangedPage(pageData: PageEvent) {
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+
   ngOnInit() {
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService
       .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
