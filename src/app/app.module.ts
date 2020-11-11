8456b3272ff7310cd66733e0d7f70fb4dd52933e
Cleaver Barnes <cbarnes@cleaver.ca>
2020-11-10 22:06:30 -0500

    refactor - separate angular auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AngularMaterialModule } from './angular-material.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './auth/login/login.component';
 import { PostsModule } from './posts/posts.module';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ErrorComponent,
-    HeaderComponent,
-    LoginComponent,
-    SignupComponent,
-  ],
+  declarations: [AppComponent, ErrorComponent, HeaderComponent],
   imports: [
     AngularMaterialModule,
     AppRoutingModule,
+    AuthModule,
     BrowserAnimationsModule,
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     PostsModule,
   ],
